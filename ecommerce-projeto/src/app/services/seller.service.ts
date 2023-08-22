8f94ca1680ca6e3fa8db6319c4bfc2230d7c1507
Luciano Paiva Cavalcanti <lucianocavalcantipf@icloud.com>
2023-08-22 04:27:45 -0300

    atualizando o repositorio

---
+++ ecommerce-projeto/src/app/services/seller.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { signUp } from '../data-type';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SellerService {
+  isSellerLogged = new BehaviorSubject<boolean>(false);
+  constructor(private http: HttpClient, private router: Router) { }
+  userSignUp(data: signUp) {
+    this.http
+      .post('http://localhost:3000/seller', data, { observe: 'response' })
+      .subscribe((result) => {
+        this.isSellerLogged.next(true);
+        localStorage.setItem('seller', JSON.stringify(result.body))
+        this.router.navigate(['seller-home']);
+      });
+  }
+  reloadSeller() {
+    if (localStorage.getItem('seller')) {
+      this.isSellerLogged.next(true);
+      this.router.navigate(['seller-home']);
+    }
+  }
+}
